cc3f4a4e97f6bdaed53fada847305b02d1730d9c
Dale Beran <daleberan@gmail.com>
2023-10-25 18:12:35 -0400

    Added Score UI

--- Third Person Shooter/Assets/scripts/gameStats.cs
+++ Third Person Shooter/Assets/scripts/gameStats.cs
@@ -1,8 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class gameStats : MonoBehaviour
 {
     public int Score;
+
+    public TMP_Text scoreText;
+
+    void Start()
+    {
+        Score = 0;
+    }
+
+    void FixedUpdate()
+    {
+
+        scoreText.text = "Score: " + Score.ToString();
+
+    }
+
+
 }
